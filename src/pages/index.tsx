7fc30322e2f7de2c7087c606cd3477c8d9fd7fce
Anna <ana84168@gmail.com>
2023-05-12 18:16:09 -0500

    feat: initial page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,56 @@
-import { useRouter } from "next/router";
-/* import { useEffect } from "react"; */
+import { Title, Text, Stack, Button, Center, createStyles } from "@mantine/core";
+import PlexoBackground from "components/resources/PlexoLogoBackground";
+
+const useStyles = createStyles(theme => ({
+  container: {
+    position: "relative",
+    overflow: "hidden",
+    minHeight: 600,
+    background: theme.colors.dark[7],
+    color: theme.colors.dark[0],
+  },
+  textContainer: {
+    position: "absolute",
+    bottom: 100,
+    width: "100%",
+  },
+  title: {
+    lineHeight: 1,
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 60,
+    },
+  },
+  description: {
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: 18,
+    },
+  },
+}));
 
 const Plexo = () => {
-  /* const router = useRouter(); */
+  const { classes } = useStyles();
 
-  /* useEffect(() => {
-    router.push("/tasks");
-  }, []); */
-
-  return <h1>Plexo</h1>;
+  return (
+    <Center h={"100vh"} className={classes.container}>
+      <PlexoBackground />
+      <Stack p={20} spacing={"xl"} align="center" className={classes.textContainer}>
+        <Title order={1} size={80} className={classes.title}>
+          Plexo
+        </Title>
+        <Stack spacing={0} align="center">
+          <Text size={"xl"} align="center" className={classes.description}>
+            A new kind of collaboration:
+          </Text>
+          <Text size={"xl"} align="center" className={classes.description}>
+            Open-Source Project Management System for modern innovators
+          </Text>
+        </Stack>
+        <Button size="lg" w={"fit-content"}>
+          Explore our demo
+        </Button>
+      </Stack>
+    </Center>
+  );
 };
 
 export default Plexo;
