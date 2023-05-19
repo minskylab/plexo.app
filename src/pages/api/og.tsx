5a74b265691767e7ad23c42334ec64ad7211cc6b
Anna <ana84168@gmail.com>
2023-05-19 12:14:45 -0500

    feat: og-image fonts

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -2,46 +2,60 @@ import { ImageResponse } from "@vercel/og";
 
 import { PlexoVerticalLogo } from "components/resources/ogImages/PlexoVerticalLogo";
 import { PlexoLines } from "components/resources/ogImages/PlexoLines";
+import { getFont } from "lib/ogFonts";
 
 export const config = {
   runtime: "experimental-edge",
 };
 
-const imageOg = () => {
+const imageOg = async () => {
+  // Used for most languages
+  const inter = await getFont({
+    family: "DM Sans",
+    weights: [400, 700] as const,
+  });
+
   try {
     return new ImageResponse(
       (
         <div
           style={{
             position: "relative",
-            color: "#DFDFDF",
-            backgroundColor: "#0C0C0C",
-            height: "100%",
-            width: "100%",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
             flexDirection: "column",
+            height: "100%",
+            width: "100%",
+            color: "#DFDFDF",
+            backgroundColor: "#0C0C0C",
           }}
         >
-          <div style={{ display: "flex", position: "absolute", top: 0, left: 150 }}>
+          <div style={{ position: "absolute", display: "flex", top: 0, left: 150 }}>
             <PlexoVerticalLogo />
           </div>
-          <div style={{ display: "flex", position: "absolute", top: 0, right: 150 }}>
+          <div style={{ position: "absolute", display: "flex", top: 0, right: 150 }}>
             <PlexoLines />
           </div>
           <div
             style={{
               display: "flex",
               flexDirection: "column",
-              gap: "1rem",
               marginLeft: 200,
-              width: 550,
+              width: 600,
               textAlign: "left",
             }}
           >
-            <h1 style={{ fontSize: 130, fontWeight: 700, lineHeight: "4rem" }}>Plexo</h1>
-            <h2 style={{ fontSize: 28 }}>
+            <h1
+              style={{
+                margin: 0,
+                fontSize: 130,
+                fontWeight: 700,
+              }}
+            >
+              Plexo
+            </h1>
+            <h2 style={{ margin: 0, fontSize: 28, fontWeight: 400 }}>
               Open-Source Project Management System for modern innovators
             </h2>
           </div>
@@ -50,6 +64,10 @@ const imageOg = () => {
       {
         width: 1200,
         height: 630,
+        fonts: [
+          { name: "DM Sans", data: inter[400], weight: 400 },
+          { name: "DM Sans", data: inter[700], weight: 700 },
+        ],
       }
     );
   } catch (e) {
